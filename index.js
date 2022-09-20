a454c4b42019c03de688ae2e6b253e0cf9808b42
ahmedMshaban <ahmed.ms.elias@gmail.com>
2022-09-20 09:52:34 +0300

    Render added food

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import FetchWrapper from "./fetch-wrapper.js";
+
 const trackerForm = document.querySelector("#create-form");
-let foodName = document.querySelector("#create-name");
-let foodCarbs = document.querySelector("#create-carbs");
-let foodProtein = document.querySelector("#create-protein");
-let foodFat = document.querySelector("#create-fat");
+const foodName = document.querySelector("#create-name");
+const foodCarbs = document.querySelector("#create-carbs");
+const foodProtein = document.querySelector("#create-protein");
+const foodFat = document.querySelector("#create-fat");
+const foodList = document.querySelector("#food-list");
 const API = new FetchWrapper(
   "https://firestore.googleapis.com/v1/projects/jsdemo-3f387/databases/(default)/documents/ahmedshaban"
 );
@@ -11,14 +13,14 @@ const API = new FetchWrapper(
 function formHandler(e) {
   e.preventDefault();
 
-  if (
-    !foodName.value ||
-    !foodCarbs.value ||
-    !foodProtein.value ||
-    !foodFat.value
-  ) {
-    return;
-  }
+  // if (
+  //   !foodName.value ||
+  //   !foodCarbs.value ||
+  //   !foodProtein.value ||
+  //   !foodFat.value
+  // ) {
+  //   return;
+  // }
 
   API.post("/", {
     fields: {
@@ -29,12 +31,38 @@ function formHandler(e) {
     },
   })
     .then((data) => {
-      if (!data.error) {
-        foodName.value = "";
-        foodCarbs.value = "";
-        foodProtein.value = "";
-        foodFat.value = "";
+      if (data.error) {
+        throw new Error(data.error);
       }
+
+      foodList.insertAdjacentHTML(
+        "beforeend",
+        `<li class="card">
+          <div>
+            <h3 class="name">${foodName.value}</h3>
+            <div class="calories">0 calories</div>
+            <ul class="macros">
+              <li class="carbs">
+                <div>Carbs</div>
+                <div class="value">${foodCarbs.value}g</div>
+              </li>
+              <li class="protein">
+                <div>Protein</div>
+                <div class="value">${foodProtein.value}g</div>
+              </li>
+              <li class="fat">
+                <div>Fat</div>
+                <div class="value">${foodFat.value}g</div>
+              </li>
+            </ul>
+          </div>
+        </li>`
+      );
+
+      foodName.value = "";
+      foodCarbs.value = "";
+      foodProtein.value = "";
+      foodFat.value = "";
     })
     .catch((error) => console.error(error));
 }
